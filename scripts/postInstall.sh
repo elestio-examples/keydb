2fa8fab93d85721e13523dfcdbb76b93e7ddb490
David Alimi <davealimi@gmail.com>
2024-04-03 15:15:32 +0300

    Adding SSL

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -13,3 +13,15 @@ sleep 30s;
 sleep 10s;
 
 curl --header "Content-Type: application/json" --request POST --data '{ "name": "localRedis", "connectionType": "STANDALONE", "host": "172.17.0.1","port": 23647,"password": "'${SOFTWARE_PASSWORD}'"}' http://172.17.0.1:8001/api/instance/
+
+
+sleep 25s;
+docker-compose down
+sudo apt-get install openssl -y
+openssl genrsa -out ./data/redis.key 2048
+openssl req -new -key ./data/redis.key -out ./data/redis.csr -subj "/CN=${DOMAIN}"
+openssl x509 -req -days 3650 -in ./data/redis.csr -signkey ./data/redis.key -out ./data/redis.crt
+cp ./data/redis.crt ./data/ca.crt 
+sleep 5s;
+sed -i 's/#--tls/--tls/' docker-compose.yml
+docker-compose up -d;
